496693a8b1bdcf61a320eef3278ebe7042b5af1c
Chris Taylor <shredder.ct@gmail.com>
2022-01-22 22:00:57 -0600

    player movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour {
   [SerializeField] private float speed = 1;
 
+  private Rigidbody2D _rigidbody2D;
   private Vector2 _moveValue;
+
+  private void Start()
+  {
+    _rigidbody2D = GetComponent<Rigidbody2D>();
+  }
 
   public void SetMoveDir(Vector2 value) => _moveValue = value.normalized;
 
-  private void Update() {
+  private void LateUpdate() {
     var moveDir = _moveValue * speed * Time.deltaTime;
-    transform.position += new Vector3(moveDir.x, moveDir.y);
+    _rigidbody2D.position += moveDir;
   }
 }
---
+++ Assets/Scripts/UI/RadialMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace DefaultNamespace.UI
+{
+    public class RadialMenu : MonoBehaviour
+    {
+        [SerializeField] private RectTransform content;
+        
+        public void Open()
+        {
+    
+
+        }
+
+        public void Close()
+        {
+            
+        }
+    }
+}
